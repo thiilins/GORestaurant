528fa8339048314a7b774d88e59a21c1d5dc58ca
Thiago Lins <taglins@gmail.com>
2021-11-11 15:11:43 -0300

    Convertendo Modal Edit Food de Classe para função

--- src/components/ModalEditFood/index.jsx
+++ src/components/ModalEditFood/index.jsx
@@ -1,30 +1,18 @@
-import { Component, createRef } from 'react';
+import {useRef} from 'react';
 import { FiCheckSquare } from 'react-icons/fi';
 
 import { Form } from './styles';
 import Modal from '../Modal';
 import Input from '../Input';
-
-class ModalEditFood extends Component {
-  constructor(props) {
-    super(props);
-
-    this.formRef = createRef()
-  }
-
-  handleSubmit = async (data) => {
-    const { setIsOpen, handleUpdateFood } = this.props;
-
-    handleUpdateFood(data);
+ 
+export default function ModalEditFood({ isOpen, setIsOpen, editingFood, handleUpdateFood}){
+ const formRef = useRef(null);
+  const  handleSubmit = async (data) => {
+   handleUpdateFood(data);
     setIsOpen();
   };
-
-  render() {
-    const { isOpen, setIsOpen, editingFood } = this.props;
-
-    return (
-      <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
-        <Form ref={this.formRef} onSubmit={this.handleSubmit} initialData={editingFood}>
+return (<Modal isOpen={isOpen} setIsOpen={setIsOpen}>
+        <Form ref={formRef} onSubmit={handleSubmit} initialData={editingFood}>
           <h1>Editar Prato</h1>
           <Input name="image" placeholder="Cole o link aqui" />
 
@@ -40,9 +28,5 @@ class ModalEditFood extends Component {
             </div>
           </button>
         </Form>
-      </Modal>
-    );
-  }
-};
-
-export default ModalEditFood;
+      </Modal>)
+}
